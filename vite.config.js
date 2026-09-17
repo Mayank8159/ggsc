ebf3e96f1a6ffc9a21d10c7dc1f6072f03de6843
Tridibesh Sen <tridibeshsen2002@gmail.com>
2026-09-18 01:45:06 +0530

    Adding event gallery  pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { app } from './api/lambda.js';
+import path from 'path';
 
 // Serve local Express app routes through the Vite dev server
 const expressBackendPlugin = () => ({
@@ -12,7 +13,11 @@ const expressBackendPlugin = () => ({
 
 export default defineConfig({
   plugins: [react(), expressBackendPlugin()],
-
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     allowedHosts: [
       "50bc-150-129-132-115.ngrok-free.app"
